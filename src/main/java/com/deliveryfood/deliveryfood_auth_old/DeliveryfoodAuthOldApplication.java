d952b2b4f1662d9d04eb17e75bdf1048d962a562
petterson <petterson@email.com>
2025-04-28 09:11:11 -0300

    Primeiro commit

---
+++ src/main/java/com/deliveryfood/deliveryfood_auth_old/DeliveryfoodAuthOldApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryfood.deliveryfood_auth_old;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfoodAuthOldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfoodAuthOldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryfood/deliveryfood_auth_old/DeliveryfoodAuthOldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryfood.deliveryfood_auth_old;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryfoodAuthOldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
