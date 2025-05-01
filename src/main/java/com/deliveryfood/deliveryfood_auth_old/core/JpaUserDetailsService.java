26b4798e145dc9fff30b26531590f0ac92cc7640
petterson <petterson@email.com>
2025-05-01 09:26:55 -0300

    Carregando as permissões concedidas na autenticação

--- src/main/java/com/deliveryfood/deliveryfood_auth_old/core/JpaUserDetailsService.java
+++ src/main/java/com/deliveryfood/deliveryfood_auth_old/core/JpaUserDetailsService.java
@@ -1,10 +1,16 @@
 package com.deliveryfood.deliveryfood_auth_old.core;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.deliveryfood.deliveryfood_auth_old.domain.Usuario;
 import com.deliveryfood.deliveryfood_auth_old.domain.UsuarioRepository;
@@ -15,12 +21,22 @@ public class JpaUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Transactional(readOnly = true) // para não fechar o entityManager, pois existe um escopo de transação
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = usuarioRepository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail informado"));
 		
-		return new AuthUser(usuario);
+		return new AuthUser(usuario, getAuthorities(usuario));
+	}
+
+	private Collection<GrantedAuthority> getAuthorities(Usuario usuario) {
+		return usuario.getGrupos().stream()
+				.flatMap(grupo -> grupo.getPermissoes().stream())
+				.map(permissao -> new SimpleGrantedAuthority(permissao.getNome().toUpperCase()))
+				.collect(Collectors.toSet()); // foi optado por set para evitar duplicação, um usuário em mais de um
+												// grupo pode conter a mesma permissão por grupos distintos, o que é
+												// redundante
 	}
 
 }
--- src/main/java/com/deliveryfood/deliveryfood_auth_old/domain/Usuario.java
+++ src/main/java/com/deliveryfood/deliveryfood_auth_old/domain/Usuario.java
@@ -1,10 +1,16 @@
 package com.deliveryfood.deliveryfood_auth_old.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -27,5 +33,11 @@ public class Usuario {
 	
 	@Column(nullable = false)
 	private String senha;
+
+	@ManyToMany
+    @JoinTable(name = "usuario_grupo",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private Set<Grupo> grupos = new HashSet<>();
 	
 }
